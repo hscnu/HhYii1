9d4be77a0f3e036b9582f06a439f964f9f5dd2e0
Zzlum <1594012009@qq.com>
2021-07-26 15:33:03 +0800

    改了控制器

--- admin/controllers/ResidenceController.php
+++ admin/controllers/ResidenceController.php
@@ -41,8 +41,30 @@ class ResidenceController extends BaseController {
         }
     }
 
+    public function actionDetail($id) {
+        $modelName = $this->model;
+        $model = $this->loadModel($id, $modelName);
+        if (!Yii::app()->request->isPostRequest) {
+            $data = array();
+            $data['model'] = $model;
+            $this->render('detail', $data);
+        } else {
+            $this->saveData($model, $_POST[$modelName]);
+        }
+    }
+
+    public function actionqrsh($id,$keywords=''){
+        $tmp = Residence::model()->find('id='.$id);
+        $tmp->state = '已审核';
+        $tmp->save();
+        $this->actionIndex_Residence_Examine($keywords);
+    }
+
 
     function saveData($model, $post) {
+        put_msg('65');
+        put_msg($post);
+        put_msg($_POST);
         $model->attributes = $post;
         show_status($model->save(), '保存成功', get_cookie('_currentUrl_'), '保存失败');
     }
@@ -87,15 +109,34 @@ class ResidenceController extends BaseController {
         parent::_list($model, $criteria, 'index_residence_examine', $data);
     }
 
-    public function actionIndex_Residence_List($keywords = '',$start_date='',$end_date='') {
+    public function actionIndex_Residence_List($keywords = '',$start_date='',$end_date='',$province='',$city='',$area='') {
         set_cookie('_currentUrl_', Yii::app()->request->url);
         $modelName = $this->model;
         $model = $modelName::model();
         $criteria = new CDbCriteria;
+        $criteria->condition = 'club_type=8 and if_del=510 and unit_state=648';
         $criteria -> condition = get_like('1','apply_unit_or_apply_person,account_number',$keywords);
         $criteria -> condition = get_like( $criteria -> condition,'apply_unit_or_apply_person,account_number',$keywords);
         $criteria->condition=get_where($criteria->condition,($start_date!=""),'uDate>=operation_time',$start_date,'"');
         $criteria->condition=get_where($criteria->condition,($end_date!=""),'uDate<=residence_time',$end_date,'"');
+        if($province !== ''){
+            $criteria->condition.=' AND club_area_province like "%' . $province . '%"';
+        }
+
+        if ($city == '市辖区' || $city == '市辖县' || $city == '省直辖县级行政区划') {
+            $city = '';
+        }
+        if ($area == '市辖区' || $area == '市辖县' || $area == '省直辖县级行政区划') {
+            $area = '';
+        }
+
+        if ($city != '') {
+            $criteria->condition.=' AND (club_area_city like "%' . $city . '%" or club_area_district like "%' . $city . '%" or club_area_township like "%' . $city . '%")';
+        }
+
+        if ($area != '') {
+            $criteria->condition.=' AND ( club_area_city like "%' . $area . '%" or club_area_district like "%' . $area . '%" or club_area_township like "%' . $area . '%")';
+        }
         $data = array();
         parent::_list($model, $criteria, 'index_residence_list', $data);
     }
@@ -111,7 +152,11 @@ class ResidenceController extends BaseController {
         $data = array();
         parent::_list($model, $criteria, 'index_examine_fail_list', $data);
     }
+
 }
+
+
+
 
 
 
