d7580efad9016f85f084bd78a83ea2aba03e26ec
Petersun-avi <81967667+Petersun-avi@users.noreply.github.com>
2021-07-28 21:57:18 +0800

    增加了审核、收藏功能，以及针对不同角色的action

--- admin/controllers/RestaurantController.php
+++ admin/controllers/RestaurantController.php
@@ -1,5 +1,6 @@
 <?php
 
+
 class RestaurantController extends BaseController {
 
     protected $model = '';
@@ -27,8 +28,31 @@ class RestaurantController extends BaseController {
             $this->saveData($model, $_POST[$modelName]);
         }
     }
+    public function actionUpload($id,$keywords = '') {
+        $tmp=Restaurant::model()->find('id = '.$id);
+        $tmp->r_isupload='0';
+        $tmp->r_ispass='1';
+        $tmp->save();
+        $this->actionRest($keywords);
+    }
 
-
+    public function actionPass($id,$keywords = '') {
+        $tmp=Restaurant::model()->find('id = '.$id);
+        $tmp->r_isupload='1';
+        $tmp->r_ispass='1';
+        $tmp->save();
+        $this->actionIndex($keywords);
+    }
+    public function actionReturn($keywords = '') {
+        $id = $_POST['id'];
+        $reason = $_POST['reason'];
+        $tmp=Restaurant::model()->find('id = '.$id);
+        $tmp->r_isupload='1';
+        $tmp->r_ispass='0';
+        $tmp->r_reason=$reason;
+        $tmp->save();
+        $this->actionIndex($keywords);
+    }
     public function actionUpdate($id) {
         $modelName = $this->model;
         $model = $this->loadModel($id, $modelName);
@@ -40,26 +64,79 @@ class RestaurantController extends BaseController {
             $this->saveData($model, $_POST[$modelName]);
         }
     }
-
-
+    public function actionChange($id) {
+        $modelName = $this->model;
+        $model = $this->loadModel($id, $modelName);
+        $model->r_ispass = '0';
+        $model->r_isupload = '0';
+        if (!Yii::app()->request->isPostRequest) {
+            $data = array();
+            $data['model'] = $model;
+            $this->render('change', $data);
+        } else {
+            $this->saveData($model, $_POST[$modelName]);
+        }
+    }
+    public function actionStation($rest)
+    {
+        $_SESSION['rest'] = $rest;
+        $this->redirect(array('/dish/rest'));
+    }
+    public function actionStation1($rest)
+    {
+        $_SESSION['rest'] = $rest;
+        $this->redirect(array('/dish/user'));
+    }
     function saveData($model, $post) {
         $model->attributes = $post;
-        //   put_msg($post);
-        //  put_msg($model->attributes);
-
         show_status($model->save(), '保存成功', get_cookie('_currentUrl_'), '保存失败');
     }
-
+    public function actionSaveLike() {
+        $tmp = Restaurant::model()->find(array(
+            'select'=>array('id,r_name'),
+            'condition' => 'r_name=:r_name',
+            'params' => array('r_name'=>$_SESSION['rest']),
+        ));
+        $id = $tmp->id;
+        $tmp=Restaurant::model()->find('id = '.$id);
+        $tmp->r_like = $_POST['isLike'];
+        $tmp->save();
+    }
     //列表搜索
     public function actionIndex($keywords = '') {
         set_cookie('_currentUrl_', Yii::app()->request->url);
         $modelName = $this->model;
         $model = $modelName::model();
         $criteria = new CDbCriteria;
-        $criteria -> condition = get_like('1','r_name,r_address,r_phone',$keywords);
-        $criteria -> condition = get_like( $criteria -> condition,'r_name,r_address,r_phone',$keywords);
+        $criteria -> condition = get_like('1','r_name,r_address',$keywords);
+        $criteria -> condition = get_like( $criteria -> condition,'r_name,r_address',$keywords);
 
         $data = array();
         parent::_list($model, $criteria, 'index', $data);
+    }
+
+
+    public function actionUser($keywords = '') {
+        set_cookie('_currentUrl_', Yii::app()->request->url);
+        $modelName = $this->model;
+        $model = $modelName::model();
+        $criteria = new CDbCriteria;
+        $criteria -> condition = get_like('1','r_name,r_address',$keywords);
+        $criteria -> condition = get_like( $criteria -> condition,'r_name,r_address',$keywords);
+
+        $data = array();
+        parent::_list($model, $criteria, 'user', $data);
+    }
+
+    public function actionRest($keywords = '') {
+        set_cookie('_currentUrl_', Yii::app()->request->url);
+        $modelName = $this->model;
+        $model = $modelName::model();
+        $criteria = new CDbCriteria;
+        $criteria -> condition = get_like('1','r_name,r_address',$keywords);
+        $criteria -> condition = get_like( $criteria -> condition,'r_name,r_address',$keywords);
+
+        $data = array();
+        parent::_list($model, $criteria, 'rest', $data);
     }
 }
